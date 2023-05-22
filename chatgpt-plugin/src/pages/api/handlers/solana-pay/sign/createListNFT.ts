6ff0226219d9fee337d7fc85d7a81946e44c91d8
ngundotra <noah@gundotra.org>
2023-05-22 11:03:35 -0400

    add nft listing

--- chatgpt-plugin/src/pages/api/handlers/solana-pay/sign/createListNFT.ts
+++ chatgpt-plugin/src/pages/api/handlers/solana-pay/sign/createListNFT.ts
@@ -5,21 +5,21 @@ import { base64 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 import { HYPERSPACE_CLIENT } from "../../../constants";
 import { makeRespondToSolanaPayPost, makeRespondToSolanaPayGet } from ".";
 
-async function hyperspaceCreateBuyTx(
-  buyer: string,
+async function hyperspaceCreateListTx(
+  seller: string,
   token: string,
   price: number
 ) {
-  let transactionData = await HYPERSPACE_CLIENT.createBuyTx({
-    buyerAddress: buyer,
+  let transactionData = await HYPERSPACE_CLIENT.createListTx({
+    sellerAddress: seller,
     tokenAddress: token,
     price: price,
     // Take no fee on making tx for ChatGPT users
-    buyerBroker: "",
-    buyerBrokerBasisPoints: 0,
+    sellerBroker: "",
+    sellerBrokerBasisPoints: 0,
   });
   const txBytes = base64.encode(
-    Buffer.from(transactionData.createBuyTx.stdBuffer!)
+    Buffer.from(transactionData.createListTx.stdBuffer!)
   );
 
   return {
@@ -27,16 +27,16 @@ async function hyperspaceCreateBuyTx(
   };
 }
 
-export async function createBuyNFT(req: NextApiRequest) {
+export async function createListNFT(req: NextApiRequest) {
   const { token, price } = req.query;
-  const { account: buyer } = req.body;
-  return await hyperspaceCreateBuyTx(
-    buyer as string,
+  const { account: seller } = req.body;
+  return await hyperspaceCreateListTx(
+    seller as string,
     token as string,
     Number.parseFloat(price as string)
   );
 }
 
 export default makeRespondToSolanaPayGet(
-  makeRespondToSolanaPayPost(createBuyNFT)
+  makeRespondToSolanaPayPost(createListNFT)
 );
