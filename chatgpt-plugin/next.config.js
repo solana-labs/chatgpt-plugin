1d526b353d3855342793bb0721f565597ae8253f
ngundotra <noah@gundotra.org>
2023-05-22 12:20:29 -0400

    remove UI, redirect to AI page

--- chatgpt-plugin/next.config.js
+++ chatgpt-plugin/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://solana.com/developers/ai",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
