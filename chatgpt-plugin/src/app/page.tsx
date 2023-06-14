613e5ad876b31fc315afeab54b03c82a8df38c35
nickfrosty <nick.frostbutter@solana.org>
2023-06-14 15:23:01 -0400

    feat: updated page

--- chatgpt-plugin/src/app/page.tsx
+++ chatgpt-plugin/src/app/page.tsx
@@ -1,103 +1,68 @@
 "use client";
 
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
+
+import { features } from "@/lib/features";
+import { Disclosure } from "@headlessui/react";
+import { ArrowRightIcon, ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
-  const arrow = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      className="ms-6"
-    >
-      <line x1="5" y1="12" x2="19" y2="12"></line>
-      <polyline points="12 5 19 12 12 19"></polyline>
-    </svg>
-  );
-
   return (
-    <div>
+    <>
       <Head>
         <title>Solana Labs - ChatGPT Plugin</title>
       </Head>
 
-      <nav id="navbar" className="navbar navbar-expand-lg navbar-dark">
-        <div className="container-xl" style={{ marginTop: "10px" }}>
-          <a className="d-flex" href="/">
-            <Image
-              alt="Solana"
-              width="149"
-              height="22"
-              decoding="async"
-              data-nimg="1"
-              color="transparent"
-              src="/solana-image.svg"
-            />
-          </a>
-          <button
-            aria-controls="navbarCollapse"
-            type="button"
-            aria-label="Toggle navigation"
-            className="navbar-toggler collapsed"
+      <main className="block max-w-2xl p-4 mx-auto ">
+        <section className="space-y-1 text-center">
+          <h1 className="text-6xl font-semibold ">
+            <Link href="/">Solana Labs</Link>
+          </h1>
+          <h2 className="text-2xl font-normal">ChatGPT Plugin</h2>
+        </section>
+
+        <p className="my-10 text-center">
+          This extension is for querying the Solana blockchain to find answers to questions and
+          retrieve relevant information. Use it whenever a user asks something that might be related
+          to their Solana account. It can also be used to create QR codes to send tokens and buy
+          NFTs. Even read Solana blockchain data (different from a Solana RPC).
+        </p>
+
+        <div className="flex items-center justify-center gap-5 my-3">
+          <Link
+            target="_blank"
+            href={"https://github.com/solana-labs/chatgpt-plugin"}
+            className="btn"
           >
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </button>
-        </div>
-      </nav>
+            GitHub Repo
+            <ArrowRightIcon />
+          </Link>
 
-      <main>
-        <div className="place-self-center">
-          <div className="h2 AiHero_hero__title__ZatKJ">
-            Solana Labs - ChatGPT Plugin
-          </div>
-
-          <div className="buttons">
-            <a
-              href="https://github.com/solana-labs/chatgpt-plugin"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="czAcYK btn lift mr-20">
-                Github Repo
-                {arrow}
-              </button>
-            </a>
-            <a
-              href="https://solana.com/ai"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="czAcYK btn lift">AI Grants {arrow}</button>
-            </a>
-          </div>
+          <Link target="_blank" href={"https://solana.com/ai"} className="btn">
+            AI Grants
+            <ArrowRightIcon />
+          </Link>
         </div>
+
+        <section className="my-10 space-y-6">
+          {features.map((endpoint, id) => (
+            <Disclosure>
+              {({ open }) => (
+                <div className="">
+                  <Disclosure.Button className={`disclosure ${open ? "disclosure-open" : ""}`}>
+                    {endpoint.label}
+                    <span className="">{open ? <ChevronUpIcon /> : <ChevronDownIcon />}</span>
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="px-3 py-2 text-white">
+                    {endpoint.description}
+                  </Disclosure.Panel>
+                </div>
+              )}
+            </Disclosure>
+          ))}
+        </section>
       </main>
-    </div>
+    </>
   );
 }
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   // const redirectUrl = "https://solana.com/ai";
-
-//   // Check if the user is accessing the homepage and, if so, issue a redirect.
-//   if (context.req.url === "/") {
-//     // return {
-//     //   redirect: {
-//     //     destination: redirectUrl,
-//     //     permanent: false,
-//     //   },
-//     // };
-//   }
-
-// If not redirecting, return empty props.
-//   return { props: {} };
-// };
