02f3bebf79eaf87c7fc15f32a2597dbc5606dd29
Varun Goyal <varungoyal23@gmail.com>
2023-08-14 16:52:22 -0500

    pr updates

--- chatgpt-plugin/src/pages/api/handlers/solana-pay/sign/createMintNFT.ts
+++ chatgpt-plugin/src/pages/api/handlers/solana-pay/sign/createMintNFT.ts
@@ -1,33 +1,27 @@
-import { NextApiRequest } from "next";
-import {
-    PublicKey
-  } from "@solana/web3.js";
-import { makeRespondToSolanaPayGet, makeRespondToSolanaPayPost } from ".";
 import { Metaplex } from "@metaplex-foundation/js";
+import { PublicKey } from "@solana/web3.js";
+import { NextApiRequest } from "next";
+import { makeRespondToSolanaPayGet, makeRespondToSolanaPayPost } from ".";
 import configConstants, { CONNECTION } from "../../../constants";
 configConstants();
 
-async function createMintNFT(req: NextApiRequest) { 
+async function createMintNFT(req: NextApiRequest) {
   const { name, metadataUri, sellerFee } = req.query;
   const { account: sender } = req.body;
   const metaplex = Metaplex.make(CONNECTION);
   const mintTransactionBuilder = await metaplex
-  .nfts()
-  .builders()
-  .create({
-    name: name as string,
-    uri: metadataUri as string,
-    sellerFeeBasisPoints: Number(sellerFee), // 2.5%
-  });
+    .nfts()
+    .builders()
+    .create({
+      name: name as string,
+      uri: metadataUri as string,
+      sellerFeeBasisPoints: Number(sellerFee),
+    });
   const latestBlockhashWithExpiryBlockHeight = await CONNECTION.getLatestBlockhash();
-  const tx = mintTransactionBuilder.toTransaction(latestBlockhashWithExpiryBlockHeight)
+  const tx = mintTransactionBuilder.toTransaction(latestBlockhashWithExpiryBlockHeight);
   tx.feePayer = new PublicKey(sender);
   return {
-      transaction: tx
-        .serialize({ requireAllSignatures: false })
-        .toString("base64"),
-    };
+    transaction: tx.serialize({ requireAllSignatures: false }).toString("base64"),
+  };
 }
-export default makeRespondToSolanaPayGet(
-  makeRespondToSolanaPayPost(createMintNFT)
-);
+export default makeRespondToSolanaPayGet(makeRespondToSolanaPayPost(createMintNFT));
