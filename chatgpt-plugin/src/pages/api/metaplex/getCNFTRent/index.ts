2e751ce002652d798b6cdd98296ae1ffdab4b5d2
Varun Goyal <varungoyal23@gmail.com>
2023-08-14 18:42:38 -0500

    size updates in getcnft rent

--- chatgpt-plugin/src/pages/api/metaplex/getCNFTRent/index.ts
+++ chatgpt-plugin/src/pages/api/metaplex/getCNFTRent/index.ts
@@ -1,21 +1,25 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import {
+  ALL_DEPTH_SIZE_PAIRS,
+  DepthSizePair,
+  getConcurrentMerkleTreeAccountSize,
+} from "@solana/spl-account-compression";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { NextApiRequest, NextApiResponse } from "next";
 import configConstants, { CONNECTION } from "../../constants";
-import { getConcurrentMerkleTreeAccountSize, ALL_DEPTH_SIZE_PAIRS, DepthSizePair } from "@solana/spl-account-compression";
 configConstants();
 
 enum TreeSizes {
   Custom,
-  Small, 
+  Small,
   Medium,
-  Large
+  Large,
 }
-enum CanopySizes { 
+enum CanopySizes {
   Custom,
   Small,
-  Medium, 
+  Medium,
   Large,
-  None
+  None,
 }
 
 function isValidDepthSizePair(maxDepth: number, maxBufferSize: number): boolean {
@@ -23,42 +27,35 @@ function isValidDepthSizePair(maxDepth: number, maxBufferSize: number): boolean 
     maxDepth,
     maxBufferSize,
   };
-  return ALL_DEPTH_SIZE_PAIRS.some((validPair) =>
-    isEqualDepthSizePair(validPair, pair)
-  );
+  return ALL_DEPTH_SIZE_PAIRS.some(validPair => isEqualDepthSizePair(validPair, pair));
 }
 
 function isEqualDepthSizePair(pair1: DepthSizePair, pair2: DepthSizePair): boolean {
   return pair1.maxDepth === pair2.maxDepth && pair1.maxBufferSize === pair2.maxBufferSize;
 }
 
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method != "POST") {
     res.status(405).send({ message: "Only POST requests allowed" });
     return;
   }
 
-  const { treeSize, canopySize, customMaxDepth, customMaxBufferSize, customCanopyDepth} = req.body;
-  let maxDepth : number, maxBufferSize : number, canopyDepth : number;
-  console.log("treeSize", treeSize == TreeSizes.Small);
-  if (treeSize == TreeSizes.Custom) { 
+  const { treeSize, canopySize, customMaxDepth, customMaxBufferSize, customCanopyDepth } = req.body;
+  let maxDepth: number, maxBufferSize: number, canopyDepth: number;
+  if (treeSize.toLowerCase() == "custom") {
     if (customMaxDepth == undefined || customMaxBufferSize == undefined) {
       res.status(400).send({ message: "Missing tree size custom parameters" });
       return;
     }
     maxDepth = customMaxDepth;
     maxBufferSize = customMaxBufferSize;
-  } else if (treeSize == TreeSizes.Small) {
+  } else if (treeSize.toLowerCase() == "small") {
     maxDepth = 14;
     maxBufferSize = 64;
-  } else if (treeSize == TreeSizes.Medium) {
+  } else if (treeSize.toLowerCase() == "medium") {
     maxDepth = 20;
     maxBufferSize = 64;
-  } else if (treeSize == TreeSizes.Large) {
+  } else if (treeSize.toLowerCase() == "large") {
     maxDepth = 30;
     maxBufferSize = 256;
   } else {
@@ -71,31 +68,27 @@ export default async function handler(
     return;
   }
 
-  if (canopySize == CanopySizes.Custom) {
+  if (canopySize.toLowerCase() == "custom") {
     if (customCanopyDepth == undefined) {
       res.status(400).send({ message: "Missing custom canopy depth parameter" });
       return;
     }
     canopyDepth = customCanopyDepth;
-  } else if (canopySize == CanopySizes.Small) {
+  } else if (canopySize.toLowerCase() == "small") {
     canopyDepth = 3;
-  } else if (canopySize == CanopySizes.Medium) {
+  } else if (canopySize.toLowerCase() == "medium") {
     canopyDepth = 14;
-  } else if (canopySize == CanopySizes.Large) {
+  } else if (canopySize.toLowerCase() == "large") {
     canopyDepth = 17;
-  } else if (canopySize == CanopySizes.None) {
+  } else if (canopySize.toLowerCase() == "none") {
     canopyDepth = 0;
   } else {
     res.status(400).send({ message: "Invalid canopy size enum" });
     return;
   }
 
-  const requiredSpace = getConcurrentMerkleTreeAccountSize(
-    maxDepth,
-    maxBufferSize,
-    canopyDepth
-  );
+  const requiredSpace = getConcurrentMerkleTreeAccountSize(maxDepth, maxBufferSize, canopyDepth);
 
   const rent = await CONNECTION.getMinimumBalanceForRentExemption(requiredSpace);
-  res.status(200).send({ sol: rent / LAMPORTS_PER_SOL, proofSize: maxDepth - canopyDepth, });
+  res.status(200).send({ sol: rent / LAMPORTS_PER_SOL, proofSize: maxDepth - canopyDepth });
 }
