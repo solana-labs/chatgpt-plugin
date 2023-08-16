dd565778d0b36c752f64482548f61f16d8c35632
Varun Goyal <varungoyal23@gmail.com>
2023-08-16 14:14:04 -0500

    changes on commits

--- chatgpt-plugin/src/pages/api/metaplex/getPublicTree/index.ts
+++ chatgpt-plugin/src/pages/api/metaplex/getPublicTree/index.ts
@@ -1,13 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import configConstants  from "../../constants";
-import { PublicKey } from "@solana/web3.js";
+import configConstants from "../../constants";
 configConstants();
 
-const publicTreeAddresses: PublicKey[] = [new PublicKey("3cnMCu5hezx9h6kjEYdTb7PRKch4a6KRFBoUF8SJEsT8"), new PublicKey("GNsnin9c2nDGp78E69tGXyMScWfysnu2PuxQxXy1jh3R")]
+const publicTreeAddresses: String[] = [
+  "3cnMCu5hezx9h6kjEYdTb7PRKch4a6KRFBoUF8SJEsT8",
+  "GNsnin9c2nDGp78E69tGXyMScWfysnu2PuxQxXy1jh3R",
+];
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(200).send({ publicTreeAddresses: publicTreeAddresses});
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).send({ publicTreeAddresses: publicTreeAddresses });
 }
