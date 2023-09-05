7e959be008e943848dd9eb34f99dcc807069b34e
ngundotra <noah@gundotra.org>
2023-09-05 10:47:36 -0400

    add solflare pfp plugin

---
+++ chatgpt-plugin/src/pages/api/solflare-pfp/getProfilePic.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getProfilePicture } from "@solflare-wallet/pfp";
+import configConstants, { CONNECTION } from "../constants";
+configConstants();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method != "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
+  }
+
+  let publicKey = req.body.publicKey;
+  let pfpResult = await getProfilePicture(CONNECTION, publicKey);
+  res.status(200).send(pfpResult);
+}
