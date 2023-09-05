6b5faa0349d976dc5cd7f6ea169ad8ed5b78146a
ngundotra <noah@gundotra.org>
2023-09-05 10:47:36 -0400

    add solflare pfp plugin

---
+++ chatgpt-plugin/src/pages/api/handlers/solana-pay/sign/createSetProfilePic.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest } from "next";
+import { PublicKey, Transaction, SystemProgram, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+import { makeRespondToSolanaPayGet, makeRespondToSolanaPayPost } from ".";
+import configConstants, { CONNECTION } from "../../../constants";
+import { createSetProfilePictureTransaction } from "@solflare-wallet/pfp";
+configConstants();
+
+async function createSetProfilePic(req: NextApiRequest) {
+  const { mintPublicKey, tokenAccountPublicKey } = req.query;
+  const { account: ownerAccountPublicKey } = req.body;
+
+  const tx = await createSetProfilePictureTransaction(
+    new PublicKey(ownerAccountPublicKey),
+    new PublicKey(mintPublicKey as string),
+    new PublicKey(tokenAccountPublicKey as string),
+  );
+  tx.feePayer = new PublicKey(ownerAccountPublicKey);
+  tx.recentBlockhash = (await CONNECTION.getLatestBlockhash()).blockhash;
+
+  return {
+    transaction: tx.serialize({ requireAllSignatures: false }).toString("base64"),
+  };
+}
+
+export default makeRespondToSolanaPayGet(makeRespondToSolanaPayPost(createSetProfilePic));
