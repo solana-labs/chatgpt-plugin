9c93aee3a4fe6cf7bcf8ccc8cf87d2ce013187bc
ngundotra <noah@gundotra.org>
2023-09-14 14:33:37 -0400

    clean up gsfa

--- chatgpt-plugin/src/pages/api/handlers/getSignaturesForAddress/index.ts
+++ chatgpt-plugin/src/pages/api/handlers/getSignaturesForAddress/index.ts
@@ -3,22 +3,34 @@ import { PublicKey } from "@solana/web3.js";
 import configConstants, { CONNECTION } from "../../constants";
 configConstants();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const accountAddress = new PublicKey(req.body.address);
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let accountAddress: PublicKey;
+  try {
+    accountAddress = new PublicKey(req.body.address);
+  } catch (_e) {
+    res.status(400).send({ message: "Provided address is not a valid Solana address" });
+    return;
+  }
+
+  const beforeSig = req.body.beforeSignature ?? "";
+  const untilSig = req.body.untilSignature ?? "";
   const signatures = await CONNECTION.getSignaturesForAddress(accountAddress, {
     limit: 11,
-    before: req.body.beforeSignature ?? null,
-    until: req.body.untilSignature ?? null,
+    before: beforeSig.length > 0 ? beforeSig : undefined,
+    until: untilSig.length > 0 ? untilSig : undefined,
   });
   res.status(200).send({
     hasMore: signatures.length === 11,
-    nextPage:
-      signatures.length === 11
-        ? { beforeSignature: signatures[10].signature }
-        : null,
-    signatures: JSON.stringify(signatures),
+    nextPage: signatures.length === 11 ? { beforeSignature: signatures[10].signature } : null,
+    signatures: signatures.map(sig => {
+      return {
+        slot: sig.slot,
+        signature: sig.signature,
+        err: sig.err ?? undefined,
+        memo: sig.memo ?? undefined,
+        confirmationStatus: sig.confirmationStatus,
+        blockTime: new Date((sig.blockTime as number) * 1000).toDateString(),
+      };
+    }),
   });
 }
