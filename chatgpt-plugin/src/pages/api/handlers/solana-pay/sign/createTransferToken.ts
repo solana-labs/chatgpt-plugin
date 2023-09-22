2946a993eb43e7f3c0007022723d2dadac338114
ngundotra <noah@gundotra.org>
2023-09-22 11:42:40 -0400

    use decimals

--- chatgpt-plugin/src/pages/api/handlers/solana-pay/sign/createTransferToken.ts
+++ chatgpt-plugin/src/pages/api/handlers/solana-pay/sign/createTransferToken.ts
@@ -1,6 +1,11 @@
 import { NextApiRequest } from "next";
 import { PublicKey, Transaction } from "@solana/web3.js";
-import { createTransferInstruction, getAssociatedTokenAddressSync } from "@solana/spl-token";
+import {
+  getAssociatedTokenAddressSync,
+  createTransferCheckedInstruction,
+  getMint,
+  Mint,
+} from "@solana/spl-token";
 import { makeRespondToSolanaPayGet, makeRespondToSolanaPayPost } from ".";
 import configConstants, { CONNECTION } from "../../../constants";
 configConstants();
@@ -13,12 +18,28 @@ async function createTransferToken(req: NextApiRequest) {
   const destination = req.query["destination"] as any as PublicKey;
   const mint = req.query["mint"] as any as PublicKey;
 
+  let mintAccount: Mint;
+  try {
+    mintAccount = await getMint(CONNECTION, mint, "confirmed");
+  } catch (error) {
+    throw new Error(`Mint ${mint.toString()} not found`);
+  }
+
   const sourceToken = getAssociatedTokenAddressSync(mint, sender);
   const destinationToken = getAssociatedTokenAddressSync(mint, destination);
 
   const tx = new Transaction();
+  const tokens = Math.round(Number(amount as string) * Math.pow(10, mintAccount.decimals));
+
   tx.add(
-    createTransferInstruction(sourceToken, destinationToken, sender, Number(amount as string)),
+    createTransferCheckedInstruction(
+      sourceToken,
+      mint,
+      destinationToken,
+      sender,
+      tokens,
+      mintAccount.decimals,
+    ),
   );
   tx.feePayer = sender;
   tx.recentBlockhash = (await CONNECTION.getLatestBlockhash()).blockhash;
